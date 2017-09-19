4f06ed6d83db3adbdc8950105e9d1409155f49b4
Rahim Rahman <rahim@r2integration.com>
2017-09-19 15:30:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Philip Hue Simple Library
+
+Library to be used in my Josh.ai challenge terminal application and React-Native application.
+
+This is not on npm, so package need to be added manually.
+
+```
+  "dependencies": {
+    ...
+    "simple-hue-library": "git@github.com:rahimrahman/simple-hue-library.git",
+    ...
+  }
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+var Axios = require('axios');
+var _ = require('lodash');
+
+function fetchLights(api_url, api_username) {
+    const url = `${api_url}/api/${api_username}`;
+
+    return new Promise((resolve, reject) => {
+        Axios({
+            method: 'get',
+            url
+        })
+            .then((response) => {
+                var newLights = [];
+                
+                _.map(response.data.lights, (light, id) => {
+                    var lightObj = {
+                        id,                        
+                        name: light.name,                                                
+                        on: light.state.on,
+                        brightness: light.state.bri
+                    }
+                    newLights.push(lightObj);
+                })
+
+                resolve(newLights);
+            })
+            .catch((error) => {
+                reject(error);
+            })
+    })
+};
+
+export { fetchLights };
